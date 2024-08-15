1bd21afec9fdd1a38d5db1bc3d643756abf06d12
Rob Kovacs <rkovacs@justworks.com>
2024-08-15 10:20:14 -0400

    Reorganizing files; using navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
-const CleanCSS = require("clean-css");
+const cleanCSS = require("clean-css");
+const eleventyNavigation = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyNavigation);
+	eleventyConfig.addWatchTarget("./src/assets/css/");
+	
+	eleventyConfig.addFilter("json", JSON.stringify);
+	
 	eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({level: 2}).minify(code).styles;
+		return new cleanCSS({level: 2}).minify(code).styles;
 	});
+
+
+	eleventyConfig.addPassthroughCopy("src/assets/img");
+
+	return {
+		dir: {
+		  input: "src",
+		  output: "dist"
+		}
+	  }
 };
