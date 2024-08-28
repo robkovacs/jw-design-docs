f71c414008c92f6d50dd33890fef90a5766da855
Rob Kovacs <rkovacs@justworks.com>
2024-08-28 17:18:42 -0400

    add one-off JS for select demo

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process')
 const cleanCSS = require("clean-css");
 const htmlmin = require("html-minifier-terser");
+const { minify } = require("terser");
 const { nanoid } = require("nanoid");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
@@ -61,6 +62,16 @@ module.exports = function (eleventyConfig) {
         }
 
         return content;
+    });
+
+    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
+        try {
+            const minified = await minify(code);
+            callback(null, minified.code);
+        } catch (err) {
+            console.error("Terser error: ", err);
+            callback(null, code);
+        }
     });
 
     eleventyConfig.addPassthroughCopy("src/assets/img");
