0e359ba7b5c35f6568d47be7f24bf01e5b7f8dbb
Rob Kovacs <rkovacs@justworks.com>
2024-09-05 02:03:46 -0400

    Finished toggle switch styling

--- src/assets/js/toggle-switch.js
+++ src/assets/js/toggle-switch.js
@@ -1,7 +1,19 @@
+const toggleSwitchOnClick = function(e) {
+    const currentState = e.target.getAttribute("aria-checked") === "true";
+    const newState = String(!currentState);
+    e.target.setAttribute("aria-checked", newState);
+}
+
 document.querySelectorAll('.toggle-switch').forEach((toggleSwitch) => {
-    toggleSwitch.addEventListener("click", (e) => {
-        const currentState = e.target.getAttribute("aria-checked") === "true";
-        const newState = String(!currentState);
-        e.target.setAttribute("aria-checked", newState);
-    });
+    toggleSwitch.addEventListener("click", toggleSwitchOnClick);
+});
+
+/*
+    Disable aria-disabled toggle switches
+    Note that in real life there might be other earlier click events on here that these won't catch
+*/
+let disabledToggleSwitches = document.querySelectorAll('.toggle-switch[aria-disabled="true"]');
+
+disabledToggleSwitches.forEach((toggleSwitch) => {
+    toggleSwitch.removeEventListener('click', toggleSwitchOnClick);
 });
