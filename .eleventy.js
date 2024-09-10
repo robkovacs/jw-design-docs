38dd92eda19ab7482c5fca098cfebc0abc1bb6b2
Rob Kovacs <rkovacs@justworks.com>
2024-09-10 17:14:13 -0400

    update index pages to use excerpts

--- .eleventy.js
+++ .eleventy.js
@@ -67,6 +67,45 @@ module.exports = function (eleventyConfig) {
         return content;
     });
 
+
+    function findNavigationEntries(nodes = [], key = "") {
+        let pages = [];
+        
+        for (let entry of nodes) {
+            if (entry.data && entry.data.eleventyNavigation) {
+                let nav = entry.data.eleventyNavigation;
+                if (!key && !nav.parent || nav.parent === key) {
+                    pages.push(Object.assign(
+                        {},
+                        nav,
+                        {
+                            url: nav.url || entry.data.page.url,
+                            excerpt: nav.excerpt || entry.data.page.excerpt,
+                            pluginType: "eleventy-navigation"
+                        },
+                        key ? { parentKey: key } : {}
+                    ));
+                }
+            }
+        }
+
+        return pages.sort(function(a, b) {
+            return (a.order || 0) - (b.order || 0);
+        }).map(function (entry) {
+            if (!entry.title) {
+                entry.title = entry.key;
+            }
+            if (entry.key) {
+                entry.children = findNavigationEntries(nodes, entry.key);
+            }
+            return entry;
+        });
+    }
+    eleventyConfig.addFilter("eleventyNavigationWithExcerpts", function (nodes = [], key = "") {
+        return findNavigationEntries(nodes, key);
+    });
+
+
     eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
         try {
             const minified = await minify(code);
