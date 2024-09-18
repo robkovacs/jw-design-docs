63f8dbeffa60f55767c1408e9d64599dcd309117
Rob Kovacs <rkovacs@justworks.com>
2024-09-18 18:29:09 -0400

    tooltip fixes

--- src/assets/js/tooltip.js
+++ src/assets/js/tooltip.js
@@ -1,84 +1,121 @@
 import {
-    computePosition,
-    autoPlacement,
-    shift,
-    offset,
     arrow,
+    autoUpdate,
+    computePosition,
+    flip,
     inline,
+    offset,
+    size
 } from "@floating-ui/dom";
 
-/*
-    TODO:
-    - would like to use preferred placement if possible, *then* flip, *then* go wherever
-    - arrow can get misaligned when trigger is at the edge of the viewport
-    - autoUpdate!!
-    - clicking outside of a tooltip doesn't do anything on Mobile Safari
-*/
+class Tooltip {
+    constructor(trigger, tooltip) {
+        this.trigger = trigger;
+        this.tooltip = tooltip;
+        this.arrow = this.tooltip.querySelector(".tooltip__arrow");
+
+        this.addEventListeners();
+    }
+
+    startUpdating() {
+        if (!this.autoUpdating) {
+            this.autoUpdater = autoUpdate(this.trigger, this.tooltip, () => {
+                this.updatePosition();
+            });
+            this.autoUpdating = true;
+        }
+    }
+
+    stopUpdating() {
+        let newTooltip = this.tooltip.cloneNode(true);
+        newTooltip.removeAttribute("style");
+        newTooltip.querySelector(".tooltip__arrow").removeAttribute("style");
+        this.tooltip.remove();
+        this.autoUpdater();
+        this.trigger.after(newTooltip);
+        this.tooltip = newTooltip;
+        this.arrow = newTooltip.querySelector(".tooltip__arrow");
+        this.autoUpdating = false;
+    }
+
+    updatePosition() {
+        let placement = this.tooltip.dataset.placement ? this.tooltip.dataset.placement : 'top';
+        computePosition(this.trigger, this.tooltip, {
+            placement: placement,
+            middleware: [
+                inline(),
+                flip({
+                    fallbackAxisSideDirection: "start"
+                }),
+                offset(8),
+                size({
+                    apply({ availableWidth, availableHeight, elements }) {
+                        Object.assign(elements.floating.style, {
+                            maxWidth: `${Math.min(256, Math.max(128, availableWidth - 8))}px`,
+                            maxHeight: `${Math.min(256, Math.max(128, availableHeight - 8))}px`,
+                        });
+                    },
+                }),
+                arrow({ element: this.arrow }),
+            ],
+        }).then(({ x, y, placement, middlewareData }) => {
+            Object.assign(this.tooltip.style, {
+                left: `${x}px`,
+                top: `${y}px`,
+            });
+
+            const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+            const staticSide = {
+                top: "bottom",
+                right: "left",
+                bottom: "top",
+                left: "right",
+            }[placement.split("-")[0]];
+
+            Object.assign(this.arrow.style, {
+                left: arrowX != null ? `${arrowX}px` : "",
+                top: arrowY != null ? `${arrowY}px` : "",
+                right: "",
+                bottom: "",
+                [staticSide]: "-4px",
+            });
+        });
+    }
+
+    showTooltip() {
+        this.tooltip.style.display = "block";
+        this.startUpdating();
+    }
+
+    hideTooltip() {
+        this.tooltip.style.display = "none";
+        this.stopUpdating();
+    }
+
+    addEventListeners() {
+        this.trigger.addEventListener("mouseenter", () => {
+            this.showTooltip();
+        });
+
+        this.trigger.addEventListener("focus", () => {
+            this.showTooltip();
+        });
+
+        this.trigger.addEventListener("mouseleave", () => {
+            this.hideTooltip();
+        });
+
+        this.trigger.addEventListener("blur", () => {
+            this.hideTooltip();
+        });
+    }
+}
 
 let triggers = document.querySelectorAll(".tooltip__trigger");
 
-let hideTooltip = function (tooltip) {
-    tooltip.style.display = "none";
-};
+triggers.forEach((trigger) => {
+    let tooltip = trigger.nextElementSibling;
 
-let showTooltip = function (trigger, tooltip) {
-    tooltip.style.display = "block";
-
-    let arrowElement = tooltip.querySelector(".tooltip__arrow");
-
-    computePosition(trigger, tooltip, {
-        middleware: [
-            autoPlacement({
-                alignment: "top",
-                crossAxis: true,
-            }),
-            inline(),
-            shift({ padding: 16 }),
-            offset(8),
-            arrow({ element: arrowElement }),
-        ],
-    }).then(({ x, y, placement, middlewareData }) => {
-        Object.assign(tooltip.style, {
-            left: `${x}px`,
-            top: `${y}px`,
-        });
-
-        const { x: arrowX, y: arrowY } = middlewareData.arrow;
-
-        const staticSide = {
-            top: "bottom",
-            right: "left",
-            bottom: "top",
-            left: "right",
-        }[placement.split("-")[0]];
-
-        Object.assign(arrowElement.style, {
-            left: arrowX != null ? `${arrowX}px` : "",
-            top: arrowY != null ? `${arrowY}px` : "",
-            right: "",
-            bottom: "",
-            [staticSide]: "-4px",
-        });
-    });
-};
-
-triggers.forEach((el) => {
-    let trigger = el;
-    let tooltip = el.nextElementSibling;
-
-    trigger.addEventListener("mouseenter", () => {
-        showTooltip(trigger, tooltip);
-    });
-
-    trigger.addEventListener("focus", () => {
-        showTooltip(trigger, tooltip);
-    });
-
-    trigger.addEventListener("mouseleave", () => {
-        hideTooltip(tooltip);
-    });
-
-    trigger.addEventListener("blur", () => {
-        hideTooltip(tooltip);
-    });
+    new Tooltip(trigger, tooltip);
 });
