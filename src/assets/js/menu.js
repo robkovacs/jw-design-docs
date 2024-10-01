fb0c068be29d2375a26f6934bcc6ed4eaface633
Rob Kovacs <rkovacs@justworks.com>
2024-09-30 23:56:14 -0400

    menu a11y improvements

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -5,8 +5,8 @@ class Menu {
         this.trigger = trigger;
         this.menu = this.trigger.nextElementSibling;
         this.autoUpdating = false;
-        this.menuHidden = true;
 
+        this.setupMenuItems();        
         this.addEventListeners();
     }
 
@@ -26,13 +26,14 @@ class Menu {
         this.autoUpdater();
         this.trigger.after(newMenu);
         this.menu = newMenu;
+        this.setupMenuItems();
         this.autoUpdating = false;
     }
 
     updatePosition() {
         computePosition(this.trigger, this.menu, {
             placement: "bottom-start",
-            middleware: [flip(), size({
+            middleware: [flip({ padding: 32 }), size({
                 padding: 32,
                 apply({availableHeight, elements}) {
                     Object.assign(elements.floating.style, {
@@ -47,31 +48,46 @@ class Menu {
                 left: `${x}px`,
                 top: `${y}px`,
             });
+
+            if ([...this.menuItems].indexOf(document.activeElement) !== 1) {
+                this.menu.scrollTo(0, document.activeElement.offsetTop - 4);
+            }
         });
     }
 
     hideMenu() {
         this.menu.scrollTop = 0;
-        this.menu.style.display = "none";
-        this.menuHidden = true;
+        this.menu.hidden = true;
         this.stopUpdating();
     }
 
     showMenu() {
-        this.menu.style.display = "block";
+        this.trigger.setAttribute("aria-expanded", true);
+        this.menu.hidden = false;
         this.startUpdating();
-        this.menuHidden = false;
     }
 
     addEventListeners() {
         this.trigger.addEventListener("click", () => {
             this.showMenu();
+            this.menuItems[0].focus();
         });
 
-        this.trigger.addEventListener("focus", (e) => {
-            this.showMenu();
-        });
+        this.trigger.addEventListener("keydown", (e) => {
+            if (e.key === "ArrowDown") {
+                e.preventDefault();
+                this.showMenu();
+                this.menuItems[0].focus();
+            }
 
+            if (e.key === "ArrowUp") {
+                e.preventDefault();
+                this.showMenu();
+                this.menuItems[this.menuItems.length - 1].focus();
+            }
+        });
+        
+        // this may be unnecessary?
         this.trigger.addEventListener("blur", (e) => {
             if (e.relatedTarget === null) {
                 this.hideMenu();
@@ -91,9 +107,46 @@ class Menu {
                 e.target == this.trigger ||
                 menuNodes.indexOf(e.target) !== -1 ||
                 triggerNodes.indexOf(e.target) !== -1;
-            if (!this.menuHidden && !thisMenuTargeted) {
+            if (!this.menu.hidden && !thisMenuTargeted) {
                 this.hideMenu();
             }
+        });
+    }
+
+    setupMenuItems() {
+        this.menuItems = this.menu.querySelectorAll('[role="menuitem"]');
+        this.menuItems.forEach((el) => {
+            el.addEventListener("keydown", (e) => {
+                if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
+                    e.preventDefault();
+                    if (e.target.previousElementSibling) {
+                        e.target.previousElementSibling.focus();
+                    } else {
+                        e.target.parentNode.lastElementChild.focus();
+                    }
+                }
+
+                if (e.key === "ArrowDown" || e.key === "ArrowRight") {
+                    e.preventDefault();
+                    if (e.target.nextElementSibling) {
+                        e.target.nextElementSibling.focus();
+                    } else {
+                        e.target.parentNode.firstElementChild.focus();
+                    }
+                }
+
+                if (e.key === "Escape") {
+                    e.preventDefault();
+                    this.hideMenu();
+                    this.trigger.focus();
+                }
+
+                if (e.key === "Tab" || (e.shiftKey && e.key === "Tab")) {
+                    e.preventDefault();
+                    this.hideMenu();
+                    this.trigger.focus();
+                }
+            });
         });
     }
 
